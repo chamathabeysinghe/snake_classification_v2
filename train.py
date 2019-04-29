be5108916ba00f80d5c9c5d41c66a2831703f7cb
chamathabeysinghe <abeysinghechamath@gmail.com>
2019-04-29 22:16:36 +0530

    Back to original dataset

--- train.py
+++ train.py
@@ -4,8 +4,9 @@ from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.models import load_model
 from keras.optimizers import Adam
 
-from networks.siamese_network_2.DataGenerator import DataGenerator
-from networks.siamese_network_2.SiameseModel import SiameseModel
+from data.DataGenerator import DataGenerator
+from data.ImageLoader import ImageLoader
+from model.SiameseModel import SiameseModel
 import os
 from time import time
 
@@ -22,7 +23,7 @@ if len(ckpts) != 0:
 else:
     model = SiameseModel().build()
 
-optimizer = Adam(0.000006)
+optimizer = Adam(0.000000006)
 model.compile(loss='binary_crossentropy', optimizer=optimizer)
 
 os.makedirs("checkpoints", exist_ok=True)
@@ -40,37 +41,44 @@ tensorboard = TensorBoard(log_dir="logs/{}".format(time()), histogram_freq=0)
 if n_epochs_to_train <= initial_epoch:
     n_epochs_to_train += initial_epoch
 
-training_similar_path = './train_npy/similar'
-training_different_path = './train_npy/different'
+training_similar_path = './data/dataset/train_npy/similar'
+training_different_path = './data/dataset/train_npy/different'
 training_similar_file_count = len(os.listdir(training_similar_path))
 training_different_file_count = len(os.listdir(training_different_path))
 
-val_similar_path = './val_npy/similar'
-val_different_path = './val_npy/different'
+val_similar_path = './data/dataset/val_npy/similar'
+val_different_path = './data/dataset/val_npy/different'
 val_similar_file_count = len(os.listdir(val_similar_path))
 val_different_file_count = len(os.listdir(val_different_path))
 
-training_generator = DataGenerator(training_similar_path,
-                                   training_different_path,
-                                   training_similar_file_count,
-                                   training_different_file_count,
-                                   batch_size=128)
+# training_generator = DataGenerator(training_similar_path,
+#                                    training_different_path,
+#                                    training_similar_file_count,
+#                                    training_different_file_count,
+#                                    batch_size=10)
+#
+# validation_generator = DataGenerator(val_similar_path,
+#                                      val_different_path,
+#                                      val_similar_file_count,
+#                                      val_different_file_count,
+#                                      batch_size=10)
+#
+#
+# model.fit_generator(generator=training_generator,
+#                     epochs=n_epochs_to_train,
+#                     validation_data=validation_generator,
+#                     callbacks=[checkpoint, tensorboard],
+#                     use_multiprocessing=True,
+#                     initial_epoch=initial_epoch,
+#                     workers=6)
 
-validation_generator = DataGenerator(val_similar_path,
-                                   val_different_path,
-                                   val_similar_file_count,
-                                   val_different_file_count,
-                                   batch_size=128)
-
-
-model.fit_generator(generator=training_generator,
-                    steps_per_epoch=4,
+image_loader = ImageLoader('./data/dataset')
+model.fit_generator(generator=image_loader.generate(128),
+                    steps_per_epoch=5,
                     epochs=n_epochs_to_train,
-                    validation_data=validation_generator,
+                    validation_data=image_loader.generate_val(32),
                     validation_steps=1,
                     callbacks=[checkpoint, tensorboard],
                     use_multiprocessing=True,
                     initial_epoch=initial_epoch,
                     workers=6)
-
-
